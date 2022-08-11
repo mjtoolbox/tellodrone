cda78ce3a005c8f9617f6e914d8f500bec385890
Michael Jo <michael.jo@gmail.com>
2022-08-10 19:19:57 -0800

    thread

--- Tello3backup.py
+++ Tello3backup.py
@@ -18,9 +18,7 @@ locaddr = (host,port)
 
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
 tello_address = ('192.168.10.1', 8889)
-
 sock.bind(locaddr)
 
 def recv():
